c5d0af92d17baf30d8869ca49479399c896cfc1d
nola0002 <nola0002@stud.ek.dk>
2026-03-23 14:04:29 +0100

    pictures to lower case names, and add image to mainactivty

--- app/src/main/java/com/example/composenavigation/MainActivity.kt
+++ app/src/main/java/com/example/composenavigation/MainActivity.kt
@@ -4,6 +4,7 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
@@ -11,6 +12,7 @@ import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
 import com.example.composenavigation.ui.theme.ComposeNavigationTheme
 
@@ -21,6 +23,11 @@ class MainActivity : ComponentActivity() {
         setContent {
             Column() {
 
+                Text(text = "Home")
+
+                Image(painter = painterResource(id = R.drawable.heart2hearthjerteforhomescreen),
+                    modifier = Modifier
+                    )
             }
         }
     }
